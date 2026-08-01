499ba94326970eb03c9b2782befdd9b447092182
Yashwanthgithub123 <yashwanth89710@gmail.com>
2026-08-01 23:06:52 +0530

    Configure frontend for production API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: false,
 });
-
 // Attach JWT token to every request
 api.interceptors.request.use(
   (config) => {
